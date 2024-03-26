e1d5a547cb1d414a8802c1833c4a75842a54a90e
Jacob Davis <ismyhc@gmail.com>
2024-03-26 13:48:30 -0400

    Initial Commit

---
+++ Seer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seer
+//
+//  Created by Jacob Davis on 3/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seer/SeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeerApp.swift
+//  Seer
+//
+//  Created by Jacob Davis on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
